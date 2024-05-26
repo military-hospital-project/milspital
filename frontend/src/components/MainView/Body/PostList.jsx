140e601f3e87b87ed89c5c218ef95d2071194742
Choejeonghun <saa00123@naver.com>
2024-05-26 13:53:00 +0900

    [CJH] feat: popup header

--- frontend/src/components/MainView/Body/PostList.jsx
+++ frontend/src/components/MainView/Body/PostList.jsx
@@ -3,11 +3,13 @@ import styled from 'styled-components';
 import PostListHeader from './PostListHeader';
 import PostListItem from './PostListItem';
 import post from '../../../assets/images/post.webp';
+import WritePopup from '../../../popup/WritePopup';
 
 export default function PostList() {
   const items = Array.from({ length: 30 }, (_, i) => i);
 
   const [hasOverflow, setHasOverflow] = useState(false);
+  const [isPopupOpen, setIsPopupOpen] = useState(false);
   const itemListRef = useRef(null);
 
   useEffect(() => {
@@ -23,15 +25,25 @@ export default function PostList() {
     return () => window.removeEventListener('resize', checkOverflow);
   }, []);
 
+  const openWritePopup = () => {
+    setIsPopupOpen(!isPopupOpen);
+  };
+
+  const closePopup = () => {
+    setIsPopupOpen(false);
+  };
+
   return (
     <MainContainer>
+      {isPopupOpen && <WritePopup onClick={closePopup} />}
       <PostListHeader hasOverflow={hasOverflow} />
       <ItemList ref={itemListRef}>
         {items.map((item) => (
           <PostListItem key={item} />
         ))}
-        <Image src={post} alt='post' />
+        <Image src={post} alt='post' onClick={openWritePopup} />
       </ItemList>
+      {isPopupOpen && <Overlay />}
     </MainContainer>
   );
 }
@@ -53,4 +65,15 @@ const Image = styled.img`
   bottom: 10px;
   right: 10px;
   z-index: 50;
+  cursor: pointer;
+`;
+
+const Overlay = styled.div`
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background: rgba(0, 0, 0, 0.5);
+  z-index: 49;
 `;
--- frontend/src/popup/WritePopup.jsx
+++ frontend/src/popup/WritePopup.jsx
@@ -1,7 +1,54 @@
 import React from 'react';
+import styled from 'styled-components';
+import { palette } from 'styled-tools';
+import xicon from '../assets/images/xicon.webp';
 
-function WritePopup() {
-  return <div></div>;
+export default function WritePopup({ onClick }) {
+  return (
+    <MainContainer>
+      <Content>
+        <Header>
+          <span>진료 후기 작성</span>
+          <Image src={xicon} alt='xicon' onClick={onClick} />
+        </Header>
+      </Content>
+    </MainContainer>
+  );
 }
 
-export default WritePopup;
+const MainContainer = styled.div`
+  position: fixed;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  z-index: 100;
+`;
+
+const Content = styled.div`
+  width: 1250px;
+  height: 600px;
+  border: none;
+  box-shadow: 0 0 10px 0;
+  display: flex-start;
+  justify-content: center;
+  padding: 24px;
+  background: white;
+`;
+
+const Header = styled.div`
+  width: 100%;
+  height: 30px;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  font-size: 18px;
+  color: ${palette('gray', 0)};
+  border-bottom: 1px solid ${palette('gray', 1)};
+  padding-bottom: 10px;
+`;
+
+const Image = styled.img`
+  width: 30px;
+  height: 30px;
+  cursor: pointer;
+`;
