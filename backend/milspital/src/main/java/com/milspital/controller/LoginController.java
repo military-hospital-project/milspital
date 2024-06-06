b2be0ede8cbf3b82bf8e57495d91c30c2413470e
Lee Yeseul <yes0112@naver.com>
2024-06-06 18:03:34 +0900

    feat: 로그인, 회원가입 기능 추가

---
+++ backend/milspital/src/main/java/com/milspital/controller/LoginController.java
@@ -0,0 +1,46 @@
+package com.milspital.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.milspital.dto.request.SignInReqDto;
+import com.milspital.dto.request.SignUpReqDto;
+import com.milspital.dto.response.LoginResDto;
+import com.milspital.service.LoginService;
+
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequiredArgsConstructor
+public class LoginController {
+
+	private final LoginService loginService;
+
+	/**
+	 * 로그인 요청을 처리한다.
+	 *
+	 * @param signInReqDto 로그인 요청 dto
+	 * @return LoginResDto - 200
+	 */
+	@PostMapping("/signIn")
+	public ResponseEntity<LoginResDto> signIn(@RequestBody SignInReqDto signInReqDto) {
+		return ResponseEntity.status(HttpStatus.OK)
+				.body(loginService.signIn(signInReqDto));
+	}
+
+	/**
+	 * 회원가입 요청을 처리한다.
+	 *
+	 * @param signUpReqDto 회원가입 요청 dto
+	 * @return LoginResDto - 201
+	 */
+	@PostMapping("/signUp")
+	public ResponseEntity<LoginResDto> signUp(@RequestBody SignUpReqDto signUpReqDto) {
+		return ResponseEntity.status(HttpStatus.CREATED)
+				.body(loginService.signUp(signUpReqDto));
+	}
+
+}
--- backend/milspital/src/main/java/com/milspital/repository/UserRepository.java
+++ backend/milspital/src/main/java/com/milspital/repository/UserRepository.java
@@ -1,8 +1,10 @@
 package com.milspital.repository;
 
+import java.util.Optional;
+
 import com.milspital.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User, Long> {
-
+	Optional<User> findByArmyNumber(String armyNumber);
 }
