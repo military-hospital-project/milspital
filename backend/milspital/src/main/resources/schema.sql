a662ff7e7d50e561329cf1f68e014d416c7e1aad
Choejeonghun <saa00123@naver.com>
2024-06-08 17:14:34 +0900

    [CJH] fix: gradle

--- backend/milspital/src/main/resources/schema.sql
+++ backend/milspital/src/main/resources/schema.sql
@@ -1,73 +1,72 @@
-DROP TABLE IF EXISTS Scrap;
-DROP TABLE IF EXISTS Comment;
-DROP TABLE IF EXISTS Post;
-DROP TABLE IF EXISTS Department;
-DROP TABLE IF EXISTS Hospital;
-DROP TABLE IF EXISTS User;
+DROP TABLE IF EXISTS scrap;
+DROP TABLE IF EXISTS comment;
+DROP TABLE IF EXISTS post;
+DROP TABLE IF EXISTS department;
+DROP TABLE IF EXISTS hospital;
+DROP TABLE IF EXISTS user;
 
-CREATE TABLE User (
-                      USER_ID BIGINT NOT NULL AUTO_INCREMENT,
-                      NAME VARCHAR(10) NOT NULL,
-                      ARMY_NUMBER VARCHAR(255) NOT NULL,
-                      PASSWORD VARCHAR(200) NOT NULL,
-                      NICKNAME VARCHAR(10) NOT NULL,
-                      USER_TYPE INT NOT NULL,
-                      CREATED_AT VARCHAR(14) NOT NULL,
-                      UPDATED_AT VARCHAR(14) NOT NULL,
-                      PRIMARY KEY (USER_ID)
+CREATE TABLE user (
+    user_id BIGINT NOT NULL AUTO_INCREMENT,
+    name VARCHAR(10) NOT NULL,
+    army_number VARCHAR(255) NOT NULL,
+    password VARCHAR(200) NOT NULL,
+    nickname VARCHAR(10) NOT NULL,
+    user_type INT NOT NULL,
+    created_at VARCHAR(14) NOT NULL,
+    updated_at VARCHAR(14) NOT NULL,
+    PRIMARY KEY (user_id)
 );
 
-CREATE TABLE Hospital (
-                          HOSPITAL_ID BIGINT NOT NULL AUTO_INCREMENT,
-                          HOSPITAL_NAME VARCHAR(30) NOT NULL,
-                          PHONE VARCHAR(11) NULL,
-                          ADDRESS VARCHAR(200) NULL,
-                          PRIMARY KEY (HOSPITAL_ID)
+CREATE TABLE hospital (
+    hospital_id BIGINT NOT NULL AUTO_INCREMENT,
+    hospital_name VARCHAR(30) NOT NULL,
+    phone VARCHAR(11) NULL,
+    address VARCHAR(200) NULL,
+    PRIMARY KEY (hospital_id)
 );
 
-CREATE TABLE Department (
-                            DEPARTMENT_ID BIGINT NOT NULL AUTO_INCREMENT,
-                            DEPARTMENT_NAME VARCHAR(50) NOT NULL,
-                            PRIMARY KEY (DEPARTMENT_ID)
+CREATE TABLE department (
+    department_id BIGINT NOT NULL AUTO_INCREMENT,
+    department_name VARCHAR(50) NOT NULL,
+    PRIMARY KEY (department_id)
 );
 
-CREATE TABLE Post (
-                      POST_ID BIGINT NOT NULL AUTO_INCREMENT,
-                      WRITER_ID BIGINT NOT NULL,
-                      HOSPITAL_ID BIGINT NOT NULL,
-                      DEPARTMENT_ID BIGINT NOT NULL,
-                      DISEASE_NAME TEXT NOT NULL,
-                      CAUSE_OF_DISEASE TEXT NOT NULL,
-                      CURE_PROCESS TEXT NOT NULL,
-                      TIP TEXT NULL,
-                      CREATED_AT VARCHAR(14) NOT NULL,
-                      UPDATED_AT VARCHAR(14) NOT NULL,
-                      PRIMARY KEY (POST_ID),
-                      FOREIGN KEY (WRITER_ID) REFERENCES User(USER_ID),
-                      FOREIGN KEY (HOSPITAL_ID) REFERENCES Hospital(HOSPITAL_ID),
-                      FOREIGN KEY (DEPARTMENT_ID) REFERENCES Department(DEPARTMENT_ID)
+CREATE TABLE post (
+    post_id BIGINT NOT NULL AUTO_INCREMENT,
+    writer_id BIGINT NOT NULL,
+    hospital_id BIGINT NOT NULL,
+    department_id BIGINT NOT NULL,
+    disease_name TEXT NOT NULL,
+    cause_of_disease TEXT NOT NULL,
+    cure_process TEXT NOT NULL,
+    tip TEXT NULL,
+    created_at VARCHAR(14) NOT NULL,
+    updated_at VARCHAR(14) NOT NULL,
+    PRIMARY KEY (post_id),
+    FOREIGN KEY (writer_id) REFERENCES user(user_id),
+    FOREIGN KEY (hospital_id) REFERENCES hospital(hospital_id),
+    FOREIGN KEY (department_id) REFERENCES department(department_id)
 );
 
-CREATE TABLE Comment (
-                         COMMENT_ID BIGINT NOT NULL AUTO_INCREMENT,
-                         WRITER_ID BIGINT NOT NULL,
-                         POST_ID BIGINT NOT NULL,
-                         CONTENT TEXT NOT NULL,
-                         CREATED_AT VARCHAR(14) NOT NULL,
-                         UPDATED_AT VARCHAR(14) NOT NULL,
-                         PRIMARY KEY (COMMENT_ID),
-                         FOREIGN KEY (WRITER_ID) REFERENCES User(USER_ID),
-                         FOREIGN KEY (POST_ID) REFERENCES Post(POST_ID)
+CREATE TABLE comment (
+    comment_id BIGINT NOT NULL AUTO_INCREMENT,
+    writer_id BIGINT NOT NULL,
+    post_id BIGINT NOT NULL,
+    content TEXT NOT NULL,
+    created_at VARCHAR(14) NOT NULL,
+    updated_at VARCHAR(14) NOT NULL,
+    PRIMARY KEY (comment_id),
+    FOREIGN KEY (writer_id) REFERENCES user(user_id),
+    FOREIGN KEY (post_id) REFERENCES post(post_id)
 );
 
-CREATE TABLE Scrap (
-                       SCRAP_ID BIGINT NOT NULL AUTO_INCREMENT,
-                       USER_ID BIGINT NOT NULL,
-                       POST_ID BIGINT NOT NULL,
-                       CREATED_AT VARCHAR(14) NOT NULL,
-                       UPDATED_AT VARCHAR(14) NOT NULL,
-                       PRIMARY KEY (SCRAP_ID),
-                       FOREIGN KEY (USER_ID) REFERENCES User(USER_ID),
-                       FOREIGN KEY (POST_ID) REFERENCES Post(POST_ID)
+CREATE TABLE scrap (
+    scrap_id BIGINT NOT NULL AUTO_INCREMENT,
+    user_id BIGINT NOT NULL,
+    post_id BIGINT NOT NULL,
+    created_at VARCHAR(14) NOT NULL,
+    updated_at VARCHAR(14) NOT NULL,
+    PRIMARY KEY (scrap_id),
+    FOREIGN KEY (user_id) REFERENCES user(user_id),
+    FOREIGN KEY (post_id) REFERENCES post(post_id)
 );
-
